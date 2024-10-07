31232db6f8371c4328f94251fdd3aa8d800a9514
Kross Aleksandr <krossale@fel.cvut.cz>
2024-10-07 19:40:21 +0200

    first commit

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Home</title>
+
+      <!-- Import CSS section from PHP file -->
+      <?php require "inc/_style.php" ?>
+
+   </head>
+
+   <body>
+
+      <!-- Import header section from PHP file -->
+      <?php require "inc/_header.php" ?>
+
+      <div class="main">
+        
+         <!-- background of about -->
+         <div class="heading-home"> 
+            
+            <h1>Home</h1>
+         
+         </div>
+      </div>
+
+
+
+      <!-- Import footer section from PHP file -->
+      <?php require "inc/_footer.php" ?>
+
+   </body>
+</html>
