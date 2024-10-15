d80d3c205dcb7ffedd5b75b7375b6533e0ef00ea
Kross Aleksandr <krossale@fel.cvut.cz>
2024-10-16 01:12:45 +0200

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
+<?php @require "../app/db/config.php";?>
 <!DOCTYPE html>
 <html lang="en">
    <head>
       <meta charset="UTF-8">
       <!-- Redirect to Home page -->
-      <meta http-equiv="Refresh" content="0; home.php">
+      <meta http-equiv="Refresh" content="0; <?=MODELS_PATH?>home.php">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Index</title>
-
    </head>
-
 </html>
